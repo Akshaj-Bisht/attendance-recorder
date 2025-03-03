4fb504cd568f7b23a9b0504f0efad0f99383ec3d
Akshaj-Bisht <akshajbishtgaming@gmail.com>
2025-03-03 21:42:16 +0530

    1

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
-Bun.serve({
+const server = Bun.serve({
+	port: process.env.PORT || 3000,
 	fetch: app.fetch,
 });
-console.log("server is running");
+console.log("server is running", server.port);
